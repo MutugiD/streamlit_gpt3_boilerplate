4929af8fdf85821cba8bd6a380a1ed6be5da0e88
Mutugi <dennismutugi@hotmail.com>
2023-01-10 22:33:39 +0300

    summary model addition

---
+++ model_summary.py
@@ -0,0 +1,52 @@
+import openai
+
+summary = """Write sumamry of the following: 
+---
+{input}
+---
+This is the summary: """
+
+def set_openai_key(key):
+    """Sets OpenAI key."""
+    openai.api_key = key
+    
+class TextSummary:
+    def __init__(self):
+        print("Text Summary Model Intialized--->")
+
+    def summarize(self, input_text, myKwargs={}):
+        """
+        wrapper for the API to save the input text and the summary
+        """        
+        # Arguments to send the API
+        kwargs = {
+            "model":"text-davinci-002",
+            "prompt":"Please summarize the following text:"+input_text, 
+            "temperature":0.7, 
+            "max_tokens":64,
+            "top_p":1.0, 
+            "frequency_penalty":0.0, 
+            "presence_penalty":0.0
+            
+        }
+    
+        # update the arguments
+        for kwarg in myKwargs:
+            kwargs[kwarg] = myKwargs[kwarg]
+        
+        summary = openai.Completion.create(**kwargs)["choices"][0]["text"].strip()
+        return summary
+    
+    def model_prediction(self, input, api_key):
+        """
+        wrapper for the API to save the prompt and the result
+        """
+        # Setting the OpenAI API key got from the OpenAI dashboard
+        set_openai_key(api_key)
+        output = self.summarize(summary.format(input = input))
+        return output
+
+
+
+
+
