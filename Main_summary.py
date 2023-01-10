afb370e181e771946f2b2ed1b4b3705ec6ee441e
Mutugi <dennismutugi@hotmail.com>
2023-01-10 22:31:28 +0300

    summary model

---
+++ Main_summary.py
@@ -0,0 +1,37 @@
+import streamlit as st
+from model_summary import TextSummary
+
+
+def app():
+
+    # Creating an object of prediction service
+    pred = TextSummary()
+
+    api_key = st.sidebar.text_input("APIkey", type="password")
+    # Using the streamlit cache
+    @st.cache
+    def process_prompt(input):
+
+        return pred.model_prediction(input=input.strip() , api_key=api_key)
+
+    if api_key:
+
+        # Setting up the Title
+        st.title("Write a summary based on this text")
+
+        # st.write("---")
+
+        s_example =  "Jupiter is the fifth planet from the Sun and the largest in the Solar System. It is a gas giant with a mass one-thousandth that of the Sun, but two-and-a-half times that of all the other planets in the Solar System combined. Jupiter is one of the brightest objects visible to the naked eye in the night sky, and has been known to ancient civilizations since before recorded history. It is named after the Roman god Jupiter.[19] When viewed from Earth, Jupiter can be bright enough for its reflected light to cast visible shadows,[20] and is on average the third-brightest natural object in the night sky after the Moon and Venus."
+        input = st.text_area(
+            "Use the example below or input your own text in English",
+            value=s_example,
+            max_chars=150,
+            height=100,
+        )
+
+        if st.button("Submit"):
+            with st.spinner(text="In progress"):
+                report_text = process_prompt(input)
+                st.markdown(report_text)
+    else:
+        st.error("🔑 Please enter API Key")
